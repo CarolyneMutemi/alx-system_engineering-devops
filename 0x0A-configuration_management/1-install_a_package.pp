7e153d06f015187fae9b7286eab47421d667ed00
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-11-25 01:48:34 +0300

    Werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,6 +5,11 @@ package {'python3-pip':
   provider => 'apt'
 }
 
+package {'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
+}
+
 package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
