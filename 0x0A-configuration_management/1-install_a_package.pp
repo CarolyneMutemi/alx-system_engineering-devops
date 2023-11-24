1cf42438b1241196eeda78be9e5b10b530844c3f
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2023-11-25 01:37:37 +0300

    Puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 #  Installs flask from pip3.
 
+package {'python3-pip':
+  ensure   => 'installed',
+  provider => 'apt'
+}
+
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['python3-pip']
 }
