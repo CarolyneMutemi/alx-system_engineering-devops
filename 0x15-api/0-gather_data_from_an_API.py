14de42d63fe278c6a1ad59fe72aa7c5b0a2952d8
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-01-02 16:10:14 +0300

    Zero

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Uses given REST API for a given employee ID
+and returns information about his/her TODO list.
+"""
+
+import requests
+import sys
+
+url = f'https://jsonplaceholder.typicode.com/users/{sys.argv[1]}'
+user_url = requests.get(url, timeout=5)
+name = user_url.json().get('name')
+another_url = 'https://jsonplaceholder.typicode.com/todos/'
+todo_url = requests.get(another_url, timeout=5)
+todo_list = todo_url.json()
+done = 0
+total = 0
+list_done = []
+
+for task in todo_list:
+    if task.get('userId') == int(sys.argv[1]):
+        if task.get('completed'):
+            list_done.append(task['title'])
+            done += 1
+        total += 1
+
+print(f'Employee {name} is done with tasks({done}/{total})')
+
+for title in list_done:
+    print(f'\t {title}')
