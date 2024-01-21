bd0a82fe08a9846565046ef03ae83d565516e668
CarolyneMutemi <carolmkaysmamba14@gmail.com>
2024-01-22 01:27:43 +0300

    puppet

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,16 @@
+# Debbugging Apache returning 500 error.
+
+service { 'apache2':
+  ensure => running
+}
+
+file {'/var/www/html/index.html':
+  ensure  => 'present',
+  content => '<title>Holberton &#8211; Just another WordPress site</title>
+<link rel="alternate" type="application/rss+xml" title="Holberton &raquo; Feed" href="http://127.0.0.1/?feed=rss2" />
+<link rel="alternate" type="application/rss+xml" title="Holberton &raquo; Comments Feed" href="http://127.0.0.1/?feed=comments-rss2" />
+        <div id="wp-custom-header" class="wp-custom-header"><img src="http://127.0.0.1/wp-content/themes/twentyseventeen/assets/images/header.jpg" width="2000" height="1200" alt="Holberton" /></div>  </div>
+                            <h1 class="site-title"><a href="http://127.0.0.1/" rel="home">Holberton</a></h1>
+        <p>Yet another bug by a Holberton student</p>',
+  notify  => Service['apache2']
+}
